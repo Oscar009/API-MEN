fe4f77e373043911e0bb549fb4629ef4ff2c0b98
Oscar Ortiz <oortizbarba@gmail.com>
2022-05-31 16:05:03 -0500

    api finished

--- src/routes/dataSets.routes.js
+++ src/routes/dataSets.routes.js
@@ -8,11 +8,21 @@ router.get("/", async (req, res) => {
   res.json(dataSets);
 });
 
+router.get("/:id", async (req, res) => {
+  const dataSet = await DataSet.findById(req.params.id);
+  res.json(dataSet);
+});
+
 router.post("/", async (req, res) => {
   const { data } = req.body;
   const dataSet = new DataSet({ data });
   await dataSet.save();
   res.json({ status: "DataSet saved" });
+});
+
+router.delete("/:id", async (req, res) => {
+  await DataSet.findByIdAndRemove(req.params.id);
+  res.json({ status: "DataSet deleted" });
 });
 
 module.exports = router;
